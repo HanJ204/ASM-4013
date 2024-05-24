9efee69f45d491f3870a1715e4d102d4e039a2c5
GihanxTG <congnghia0802@email.com>
2024-05-24 17:59:21 +0700

    up đại chấm bài

--- database/seeders/insertProducts.php
+++ database/seeders/insertProducts.php
@@ -22,7 +22,7 @@ class insertProducts extends Seeder
         $ON1 = ['K11', 'K13', 'F21', 'A72', 'A74', 'A76', 'A92', 'A94', 'A96'];
         $ON2 = ['Pro', ''];
         $MN = ['E10','E20','E30','E40','G21', 'G31', 'G51', 'G71', 'G200', 'G400'];
-        for ($i=0; $i<30; $i++) {
+        for ($i=0; $i<50; $i++) {
             $HN = Arr::random($HN1). ' ' .Arr::random($HN2). ' ' .Arr::random($HN3); //Random tên Huawei
             $XN = Arr::random($XN1). ' ' .Arr::random($XN2); //Random tên Xiaomi
             $ON = Arr::random($ON1). ' ' .Arr::random($ON2); //Random tên Oppo
@@ -30,8 +30,8 @@ class insertProducts extends Seeder
             DB::table('Products')->upsert([[
                 'name' => 'Apple'.' '.mt_rand(11, 15).' '.Arr::random($AN),
                 'image' => 'apple'.mt_rand(1, 5).'.png',
-                'priceSale' => mt_rand(20000000, 30000000),
-                'price' => mt_rand(30000000, 40000000),
+                'priceSale' => mt_rand(20000000, 24999999),
+                'price' => mt_rand(25000000, 30000000),
                 'quantity' => mt_rand(10000, 20000),
                 'view' => mt_rand(5000, 20000),
                 'dateSubmitted' => now(),
@@ -39,8 +39,8 @@ class insertProducts extends Seeder
             ],[
                 'name' => 'Samsung Galaxy'.' '.Arr::random($SN),
                 'image' => 'samsung'.mt_rand(1, 5).'.png',
-                'priceSale' => mt_rand(20000000, 30000000),
-                'price' => mt_rand(30000000, 40000000),
+                'priceSale' => mt_rand(20000000, 24999999),
+                'price' => mt_rand(25000000, 30000000),
                 'quantity' => mt_rand(10000, 20000),
                 'view' => mt_rand(5000, 20000),
                 'dateSubmitted' => now(),
@@ -48,8 +48,8 @@ class insertProducts extends Seeder
             ],[
                 'name' => 'Huawei'.' '.$HN,
                 'image' => 'huawei'.mt_rand(1, 5).'.png',
-                'priceSale' => mt_rand(20000000, 30000000),
-                'price' => mt_rand(30000000, 40000000),
+                'priceSale' => mt_rand(20000000, 24999999),
+                'price' => mt_rand(25000000, 30000000),
                 'quantity' => mt_rand(10000, 20000),
                 'view' => mt_rand(5000, 20000),
                 'dateSubmitted' => now(),
@@ -57,8 +57,8 @@ class insertProducts extends Seeder
             ],[
                 'name' => 'Xiaomi'.' '.$XN,
                 'image' => 'xiaomi'.mt_rand(1, 5).'.png',
-                'priceSale' => mt_rand(20000000, 30000000),
-                'price' => mt_rand(30000000, 40000000),
+                'priceSale' => mt_rand(20000000, 24999999),
+                'price' => mt_rand(25000000, 30000000),
                 'quantity' => mt_rand(10000, 20000),
                 'view' => mt_rand(5000, 20000),
                 'dateSubmitted' => now(),
@@ -66,8 +66,8 @@ class insertProducts extends Seeder
             ],[
                 'name' => 'Oppo'.' '.$ON,
                 'image' => 'oppo'.mt_rand(1, 5).'.png',
-                'priceSale' => mt_rand(20000000, 30000000),
-                'price' => mt_rand(30000000, 40000000),
+                'priceSale' => mt_rand(20000000, 24999999),
+                'price' => mt_rand(25000000, 30000000),
                 'quantity' => mt_rand(10000, 20000),
                 'view' => mt_rand(5000, 20000),
                 'dateSubmitted' => now(),
@@ -75,8 +75,8 @@ class insertProducts extends Seeder
             ],[
                 'name' => 'Vivo Y'.mt_rand(83, 95).' '.Arr::random($ON2),
                 'image' => 'vivo'.mt_rand(1, 5).'.png',
-                'priceSale' => mt_rand(20000000, 30000000),
-                'price' => mt_rand(30000000, 40000000),
+                'priceSale' => mt_rand(20000000, 24999999),
+                'price' => mt_rand(25000000, 30000000),
                 'quantity' => mt_rand(10000, 20000),
                 'view' => mt_rand(5000, 20000),
                 'dateSubmitted' => now(),
@@ -84,8 +84,8 @@ class insertProducts extends Seeder
             ],[
                 'name' => 'Motorola Moto'.' '.Arr::random($MN),
                 'image' => 'motorola'.mt_rand(1, 5).'.png',
-                'priceSale' => mt_rand(20000000, 30000000),
-                'price' => mt_rand(30000000, 40000000),
+                'priceSale' => mt_rand(20000000, 24999999),
+                'price' => mt_rand(25000000, 30000000),
                 'quantity' => mt_rand(10000, 20000),
                 'view' => mt_rand(5000, 20000),
                 'dateSubmitted' => now(),
